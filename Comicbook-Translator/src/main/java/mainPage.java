2d7966dcd994d4abe748f568df12fa852cd59e7a
thechrizilla <chrisck.chan@hotmail.com>
2018-11-17 12:45:01 -0500

    added a simple jframe

--- Comicbook-Translator/src/main/java/mainPage.java
+++ Comicbook-Translator/src/main/java/mainPage.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class mainPage extends JFrame {
 
@@ -35,8 +40,24 @@ public class mainPage extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblHiNikhilAnd = new JLabel("Hi Nikhil and Georges");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(149)
+					.addComponent(lblHiNikhilAnd)
+					.addContainerGap(153, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(114)
+					.addComponent(lblHiNikhilAnd)
+					.addContainerGap(138, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
