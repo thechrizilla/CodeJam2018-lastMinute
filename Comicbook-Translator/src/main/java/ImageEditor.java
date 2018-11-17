91cbf0ae2d05ffaae81e0e21f8270f184e3a50c5
omric <37829365+ohcu@users.noreply.github.com>
2018-11-17 14:22:11 -0500

    Created relevant classes with method signatures

---
+++ Comicbook-Translator/src/main/java/ImageEditor.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageEditor {
+
+	private BufferedImage img;
+	
+	public ImageEditor(String path){
+		try {
+			BufferedImage img = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// Find the text and return the shapes the text are in
+	public Shape[] findText(){
+		
+		return null;
+	}
+	
+	// Remove the text on the imgs by putting shapes over the text
+	public void removeText(Shape[] shapes){
+		
+	}
+	
+	// Draw the strings within the shapes onto the imgs
+	public void addText(String[] strings, Shape[] shapes){
+		
+	}
+	
+}
---
+++ Comicbook-Translator/src/main/java/Translator.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Translator {
+
+	public static void translateString(String s){
+		
+	}
+	
+}
