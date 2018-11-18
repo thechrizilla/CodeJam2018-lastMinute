3d541eb12eb0f1f890a52dcec1fed1ebe165833c
Georges Mourant <gmourant@users.noreply.github.com>
2018-11-18 01:10:46 -0500

    Version with tests enabled

--- Comicbook-Translator/src/main/java/ImageEditor.java
+++ Comicbook-Translator/src/main/java/ImageEditor.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,11 +16,13 @@ public class ImageEditor {
 
     public static void main(String[] args) {
         ImageEditor ie = new ImageEditor("src/Comicbook-Translator/calvin_and_hobbes_4.jpg");
-//        Shape[] myShapes = new Shape[1];
-//        myShapes[0] = new Rectangle(50,50,10,10);
-//        String[] myStr = new String[1];
-//        myStr[0] = "House of Potter";
-//        ie.addText(myStr, myShapes);
+        
+        // testing lines
+        Shape[] myShapes = new Shape[1];
+        myShapes[0] = new Rectangle(50,50,10,10);
+        String[] myStr = new String[1];
+        myStr[0] = "House of Potter";
+        ie.addText(myStr, myShapes);
     }
 
     public ImageEditor(String path) {
@@ -44,7 +47,7 @@ public class ImageEditor {
         for (Shape shape : shapes) {
             graphic.fill(shape);
         }
-        //saveImgAsFile();
+        saveImgAsFile(); // only necessary if you want to save the new file now 
     }
 
     // Draw the strings within the shapes onto the imgs
@@ -60,7 +63,7 @@ public class ImageEditor {
             Point location = shapes[i].getBounds().getLocation();
             graphic.drawString(strings[i], (int)location.getX(), (int)location.getY());
         }
-        //saveImgAsFile();
+        saveImgAsFile(); // only necessary if you want to save the new file now 
     }
     
     private void saveImgAsFile(){
