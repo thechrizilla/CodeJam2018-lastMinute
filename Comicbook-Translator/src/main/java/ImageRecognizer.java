4713d7df9010afce5a7d96708b576928d3f52825
omric <37829365+ohcu@users.noreply.github.com>
2018-11-18 04:35:23 -0500

    - worked on findCountours

--- Comicbook-Translator/src/main/java/ImageRecognizer.java
+++ Comicbook-Translator/src/main/java/ImageRecognizer.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.image.BufferedImage;
@@ -12,27 +14,35 @@ import javax.imageio.ImageIO;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import net.sourceforge.tess4j.*;
 
 public class ImageRecognizer {
 
+	private String path;
 	private BufferedImage srcImg;
 	private BufferedImage gsImg;
-	private String path;
+	
 	private Mat srcGray = new Mat();
     private int threshold = 100;
     
+    private ArrayList<Shape> bubbles;
     private ArrayList<String> words;
     private ArrayList<Shape> boundingBoxes;
 	
 	public ImageRecognizer(String path){
+	    boundingBoxes = new ArrayList<Shape>();
+	    words = new ArrayList<String>();
 		try {
 			this.path = path;
 			BufferedImage img = ImageIO.read(new File(path));
+			this.srcImg = img;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,10 +52,11 @@ public class ImageRecognizer {
 	public static void main(String[] args){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		ImageRecognizer ie = new ImageRecognizer("46458735_326815428111627_254870742393421824_n.png");
+		ImageRecognizer ie = new ImageRecognizer("test4.jpg");
 		try {
-			ie.findText();
-		} catch (IOException e) {
+			
+			ie.findContours2();
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -55,20 +66,88 @@ public class ImageRecognizer {
 	
 	
 	public void findContours(){
-		Mat src = Imgcodecs.imread("path/to/img", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
-		
-        Imgproc.cvtColor(src, srcGray, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.blur(srcGray, srcGray, new Size(3, 3));
+		// Matrix
+		Mat src = Imgcodecs.imread(path, Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+		// Grayscale the image
+        // Imgproc.cvtColor(src, srcGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.blur(src, src, new Size(3, 3));
         Mat cannyOutput = new Mat();
-        Imgproc.Canny(srcGray, cannyOutput, threshold, threshold * 2);
+        Mat blurred = new Mat();
+        Mat thresh = new Mat();
+                		
+        Imgproc.GaussianBlur(src, blurred, new Size(5,5), 0.0);
+        Imgproc.threshold(blurred, thresh, 60, 255, Imgproc.THRESH_BINARY);
+        
+        Imgproc.Canny(src, cannyOutput, threshold, threshold * 2);
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
-        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
+        Imgproc.findContours(thresh, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         
+        for(MatOfPoint c : contours){
+        	Moments m = Imgproc.moments(c);
+        	double cX = m.m10 / m.m00;
+        	double cY = m.m01 / m.m00;
+        	Point p = new Point(cX, cY);
+        	
+        	
+        	Graphics2D graphic = srcImg.createGraphics();
+        	graphic.setColor(Color.RED);
+        	graphic.fill(new Rectangle((int) cX,(int) cY, 10, 10));
+        	
+        	
+        	// Imgproc.circle(src, p, 7, new Scalar(0), -1);
+        }
         
+        saveImgAsFile();
         
 	}
+	
+	public void findContours2(){
+		// Matrix
+		//Mat src = Imgcodecs.imread(path, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		Mat src = Imgcodecs.imread(path);
+		// Grayscale the image
+        // Imgproc.cvtColor(src, srcGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.blur(src, src, new Size(3, 3));
+        Mat filtered = new Mat();
+        Mat cannyOutput = new Mat();
+        Mat blurred = new Mat();
+        Mat thresh = new Mat();
+        Mat hierarchy = new Mat();
+
+        Imgproc.bilateralFilter(src, filtered, 5, 175, 175);
+        Imgproc.Canny(filtered, cannyOutput, 75, 200);
+        
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        
+        for(MatOfPoint c : contours){
+        	Moments m = Imgproc.moments(c);
+        	double cX = m.m10 / m.m00;
+        	double cY = m.m01 / m.m00;
+        	Point p = new Point(cX, cY);
+        	
+        	
+        	Graphics2D graphic = srcImg.createGraphics();
+        	graphic.setColor(Color.RED);
+        	graphic.fill(new Rectangle((int) cX,(int) cY, 10, 10));
+        	
+        	
+        	// Imgproc.circle(src, p, 7, new Scalar(0), -1);
+        }
+        
+        saveImgAsFile();
+        
+	}
+	
+	private void saveImgAsFile(){
+        try {
+            ImageIO.write(srcImg, "jpg", new File("saved.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
 
 	public Shape findBubbles(){
 		
@@ -93,6 +172,8 @@ public class ImageRecognizer {
 		}
 		System.out.println("time = " + (System.currentTimeMillis() - start));
 	}
+	
+	
 
 	public String getImgText(String imageLocation) {
 
